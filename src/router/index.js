aa2dca9d067407f674178d19ccc397f152026dc6
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2021-05-15 15:40:06 +0200

    Removed default nav from App.vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About.vue";
+import Cart from "../views/Cart.vue";
 import Nav from "../components/Nav.vue";
 import NavAdmin from "../components/NavAdmin.vue";
 
@@ -33,7 +34,7 @@ const routes = [
     path: "/cart",
     name: "Cart",
     components: {
-      // default: Cart,
+      default: Cart,
       nav: Nav,
     },
   },
---
+++ src/views/Cart.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cart">
+    <h1>Varukorg</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cart",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/main";
+
+h1 {
+  font-size: $font-heading-xl;
+  color: $dark-green;
+  margin-top: 40px;
+}
+</style>
