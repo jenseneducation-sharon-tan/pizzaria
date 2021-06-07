f5b921a822d652f472fcfd55915f41d4dd5e3c10
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2021-06-07 16:37:34 +0200

    remove codes

--- backend/app.js
+++ backend/app.js
@@ -7,11 +7,6 @@ const usersRouter = require("./routes/users");
 const adminRouter = require("./routes/admin");
 const PORT = process.env.PORT || 5000;
 //const bodyParser = require("body-parser");
-
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function(req, res) {
-  res.sendFile(__dirname + "/dist/index.html");
-});
 app.use(cors());
 app.use(express.json());
 app.use("/menu", menuRouter);
