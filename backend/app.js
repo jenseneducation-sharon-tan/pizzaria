1656d099a5a92500eae85d20d7e887be87920952
madokafujimakiahlsen <madoka.fujimakiahlsen@gmail.com>
2021-06-07 18:06:39 +0200

    no message

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,15 @@ const path = require("path");
 const PORT = process.env.PORT || 5000;
 //const bodyParser = require("body-parser");
 
-app.use(express.static(path.join(__dirname, ".", "dist")).toString());
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, ".", "dist", "index.html")).toString();
-});
-
-// app.use(express.static(path.join(__dirname, "../dist")));
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "../dist/index.html"));
+// app.use(express.static(path.join(__dirname, ".", "dist")).toString());
+// app.get(/.*/, function(req, res) {
+//   res.sendFile(path.join(__dirname, ".", "dist", "index.html")).toString();
 // });
+
+app.use(express.static(path.join(__dirname, "../dist")));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 app.use(cors());
 app.use(express.json());
